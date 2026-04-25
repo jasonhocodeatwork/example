06f9732d29dba1db10c1133a5a984f1294efd0c4
jason <jasonhocodeatwork@gmail.com>
2026-04-25 16:35:04 +0800

    crud test

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO PRODUCT (NAME, DESCRIPTION, PRICE) VALUES ('Keyboard', 'Mechanical keyboard', 79.99);
-INSERT INTO PRODUCT (NAME, DESCRIPTION, PRICE) VALUES ('Mouse', 'Wireless mouse', 29.99);
